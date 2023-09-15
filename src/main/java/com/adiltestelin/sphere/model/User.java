e8e0c0c8e97b9b6cbbd1728fc368dc1fe62f8ecf
AdilTestelin <adil.testelin59@hotmail.fr>
2023-09-15 14:19:43 +0200

    fix(registration): fix on registration

--- src/main/java/com/adiltestelin/sphere/model/User.java
+++ src/main/java/com/adiltestelin/sphere/model/User.java
@@ -13,7 +13,8 @@ import lombok.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "user_id_seq")
+    @SequenceGenerator(name = "user_id_seq", sequenceName = "user_id_seq", allocationSize = 1)
     @Column(name = "member_id")
     private Long memberId;
 
